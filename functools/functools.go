9d1be7d9e3b7a67e04139f1aaa97c0bcb7837705
Praveen Kumar <praveen+git@kumar.in>
2020-12-08 02:20:11 -0800

    Introduce unwrap construct

--- functools/functools.go
+++ functools/functools.go
@@ -1,6 +1,8 @@
 package functools
 
-import "reflect"
+import (
+	"reflect"
+)
 
 // Compose composes the given functions by passing the returned values of the
 // preceeding function as arguments to the subsequent function. This is a
@@ -13,7 +15,34 @@ func Compose(fns ...interface{}) func(args ...interface{}) {
 		}
 
 		for _, fn := range fns {
-			rargs = reflect.ValueOf(fn).Call(rargs)
+			rfn := reflect.ValueOf(fn)
+			rargs = rfn.Call(rargs)
+
+			// functools.Unwrap() returns a slice of interfaces that captures
+			// the arguments for the next function. Do some reflection magic to
+			// convert it to a slice of reflect.Value.
+			if rfn.Pointer() == reflect.ValueOf(Unwrap).Pointer() {
+				vals := rargs[0].Interface().([]interface{})
+				rargs = make([]reflect.Value, len(vals))
+				for i, val := range vals {
+					rargs[i] = reflect.ValueOf(val)
+				}
+			}
 		}
 	}
+}
+
+// Unwrap takes variadic arguments where the last argument is an error. If the
+// error is nil, it returns the rest of the arguments back. If the error is
+// non-nil, it panics.
+func Unwrap(args ...interface{}) []interface{} {
+	l := len(args)
+	if args[l-1] != nil {
+		err := args[l-1].(error)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return args[:l-1]
 }
--- functools/functools_test.go
+++ functools/functools_test.go
@@ -1,6 +1,7 @@
 package functools_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/kprav33n/aoc20/functools"
@@ -56,4 +57,76 @@ func TestCompose2(t *testing.T) {
 	require.Equal("area", arg1)
 	require.Equal(51, arg2)
 	require.InDelta(3.1416, arg3, 0.0001)
+}
+
+func TestUnaryUnwrapError(t *testing.T) {
+	require := require.New(t)
+
+	f := func() (int, error) {
+		return 14, errors.New("forced error")
+	}
+	require.Panics(func() { functools.Unwrap(f()) })
+}
+
+func TestUnaryUnwrapOK(t *testing.T) {
+	require := require.New(t)
+
+	f := func() (int, error) {
+		return 14, nil
+	}
+	result := functools.Unwrap(f())
+	require.Len(result, 1)
+	require.Equal(result[0], 14)
+}
+
+func TestBinaryUnwrapError(t *testing.T) {
+	require := require.New(t)
+
+	f := func() (string, int, error) {
+		return "emc", 2, errors.New("forced error")
+	}
+	require.Panics(func() { functools.Unwrap(f()) })
+}
+
+func TestBinaryUnwrapOK(t *testing.T) {
+	require := require.New(t)
+
+	f := func() (string, int, error) {
+		return "emc", 2, nil
+	}
+	result := functools.Unwrap(f())
+	require.Len(result, 2)
+	require.Equal(result[0], "emc")
+	require.Equal(result[1], 2)
+}
+
+func TestUnwrapComposeError(t *testing.T) {
+	require := require.New(t)
+
+	firstFunc := func() (string, error) {
+		return "", errors.New("forced error")
+	}
+
+	secondFunc := func(s string) {
+	}
+
+	require.PanicsWithError("forced error", func() {
+		functools.Compose(firstFunc, functools.Unwrap, secondFunc)()
+	})
+}
+
+func TestUnwrapComposeOK(t *testing.T) {
+	require := require.New(t)
+
+	firstFunc := func() (string, error) {
+		return "hello", nil
+	}
+
+	var arg string
+	secondFunc := func(s string) {
+		arg = s
+	}
+
+	functools.Compose(firstFunc, functools.Unwrap, secondFunc)()
+	require.Equal("hello", arg)
 }
